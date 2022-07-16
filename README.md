f14c67f8d79e3ab46f12fad3fdab243f528557ad
rockwoodc <rockwoodce@gmail.com>
2022-07-16 12:55:46 -0400

    added bootstrap cards and started 5 day forcast for loop

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # weather-dashboard
+## By Christian Rockwood
+
+This application allows the user to input a city and receive the current weather conditions, along with a five day forcast. The users input will remain in local storage and can be accessed again by clicking on the city name that was saved.
+
+![Profile](insert screen shot)
+
+(link to deployed github page)
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 var searchHistory = []
-var searchHistoryContainer = document.querySelector("#city-search");
+var searchHistoryContainer = document.getElementById("#city-search");
 var apiKey = "&appid=8cde0c178514f3d28c21cf8f9c44b5a6"
 var weatherEl = document.getElementById("forcast-info");
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city-name");
+var card1EL = document.getElementById("1");
+var card2EL = document.getElementById("2");
+var card3EL = document.getElementById("3");
+var card4EL = document.getElementById("4");
+var card5EL = document.getElementById("5");
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -26,7 +32,6 @@ var getLocation = function(city) {
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data){ 
         getWeather(data.coord);
-        
         })   
     });
     };
@@ -40,6 +45,7 @@ var getWeather = function (coord) {
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data){ 
             displayCurrent(data);
+            fiveDay(data);
         })   
     });
 }
@@ -47,24 +53,44 @@ var displayCurrent = function (weather){
     console.log(weather);
     //parse out data for temp, humidity, wind speed, & uv index
     var tempData = document.getElementById("temp");
-    tempData.textContent = weather.current.temp + " K";
+        tempData.textContent = weather.current.temp + " K";
     var windData = document.getElementById("winds");
-    windData.textContent = weather.current.wind_speed + " MPH";
+        windData.textContent = weather.current.wind_speed + " MPH";
     var humData = document.getElementById("humidity");
-    humData.textContent = weather.current.humidity + " %";
+        humData.textContent = weather.current.humidity + " %";
     var UVData = document.getElementById("uv");
-    UVData.textContent = weather.current.uvi;
-}  
-
-//color code uv index as high, medium, low
+        UVData.textContent = weather.current.uvi;
+        //color code uv index as high, medium, low
+        if (weather.current.uvi <= 2) {
+            UVData.style.backgroundColor = "rgb(103, 169, 103)";
+        }else if (weather.current.uvi <= 5) {
+            UVData.style.backgroundColor = "rgb(251, 251, 167)";    
+        }else {
+            UVData.style.backgroundColor = "rgb(247, 105, 105)";
+        };
+}
+//convert kelvin to fahrenheit
+// var kelvinToFahrenheit = fucntion(temp){
+//     1.8 * (('#temp')-273) + 32
+// }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 // show 5 day forcast
+var fiveDay = function(forcast) {
+    var fiveTemp = forcast.daily
+    for (let i=0; i<5; i++) {
+        //run through each card to populate information
+        let currentStepVar = "card" + [i + 1] +"EL";
+        currentStepVar.textContent = fiveTemp[i].temp.max;
+        console.log(currentStepVar);
+    };
+     
 
+}
 // save users searched cities
-// function getsearch() {
+// function getsearch(search) {
 //     searchHistoryContainer.innerHTML = "";
-//     console.log("hello");
+//     console.log(search);
 
 //     for (let i=0; i<searchHistory.length; i++);
 //         console.log("hello1");
@@ -76,5 +102,7 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 // function appendToSearchHistory(search) {
 //     console.log("hello2");
 //     searchHistory.push(search);
-//     localStorage.setItem("seach-history",JSON.stringify(searchHistory));
+//     localStorage.setItem("city-search",JSON.stringify(searchHistory));
 // };
+
+// getsearch();
